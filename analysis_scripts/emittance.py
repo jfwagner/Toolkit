805af5b7592cab662b5c75edd5320d70b58b5bae
Andrea <andrea.stmaria@gmail.com>
2015-08-14 11:58:31 +0200

    Finally fixing the emittance calculation :)

--- analysis_scripts/emittance.py
+++ analysis_scripts/emittance.py
@@ -1,9 +1,29 @@
+from matplotlib import pyplot as plt
+from matplotlib import rc
+from matplotlib import rcParams
 from numpy import *
 from util import *
 
+# ------------------------------------------------------------------------------
+# Define your input DUMP file
+# ------------------------------------------------------------------------------
 infile = "sixtrack/dump_ip1.txt"
 
-total_turns = 10
+# ------------------------------------------------------------------------------
+# Specify how many turns you want to read
+# ------------------------------------------------------------------------------
+total_turns = 100
+
+# ------------------------------------------------------------------------------
+# Relativistic parameters
+# ------------------------------------------------------------------------------
+gamma_rel = 7460.52280875
+beta_rel = 0.999999991017
+
+# ------------------------------------------------------------------------------
+# Initialization of arrays
+# ------------------------------------------------------------------------------
+t = zeros(total_turns)
     
 term_1_x = zeros(total_turns)
 term_2_x = zeros(total_turns)
@@ -21,30 +41,70 @@ em_x = zeros(total_turns)
 det_y = zeros(total_turns)
 em_y = zeros(total_turns)
 
+# ------------------------------------------------------------------------------
+# Extraction and treatment of data
+# ------------------------------------------------------------------------------
 for turn in range(1, total_turns + 1):
     turn_i = '%s' %turn
     x = get_dump(infile, turn_i, 3)
     xp = get_dump(infile, turn_i, 4)
     y = get_dump(infile, turn_i, 5)
     yp = get_dump(infile, turn_i, 6)
-
     # Sigma matrix for X
-    term_1_x[turn-1] = mean(dot(x,x)) - dot(mean(x), mean(x))
-    term_2_x[turn-1] = mean(dot(x,xp)) - dot(mean(x),mean(xp))
-    term_3_x[turn-1] = mean(dot(xp,x)) - dot(mean(xp), mean(x))
-    term_4_x[turn-1] = mean(dot(xp,xp)) - dot(mean(xp),mean(xp))
+    term_1_x[turn-1] = mean(multiply(x,x)) - multiply(mean(x), mean(x))
+    term_2_x[turn-1] = mean(multiply(x,xp)) - multiply(mean(x),mean(xp))
+    term_3_x[turn-1] = mean(multiply(xp,x)) - multiply(mean(xp), mean(x))
+    term_4_x[turn-1] = mean(multiply(xp,xp)) - multiply(mean(xp),mean(xp))
 
-    det_x = dot(term_1_x, term_4_x) - dot(term_3_x, term_2_x)
-    em_x[turn-1] = sqrt(abs(det_x))
+    det_x[turn-1] = multiply(term_1_x[turn-1], term_4_x[turn-1]) - multiply(term_3_x[turn-1], term_2_x[turn-1])
+    em_x[turn-1] = sqrt(abs(det_x[turn-1]))*beta_rel*gamma_rel
 
     # Sigma matrix for Y
-    term_1_y[turn-1] = mean(dot(y,y)) - dot(mean(y), mean(y))
-    term_2_y[turn-1] = mean(dot(y,yp)) - dot(mean(y),mean(yp))
-    term_3_y[turn-1] = mean(dot(yp,y)) - dot(mean(yp), mean(y))
-    term_4_y[turn-1] = mean(dot(yp,yp)) - dot(mean(yp),mean(yp))
+    term_1_y[turn-1] = mean(multiply(y,y)) - multiply(mean(y), mean(y))
+    term_2_y[turn-1] = mean(multiply(y,yp)) - multiply(mean(y),mean(yp))
+    term_3_y[turn-1] = mean(multiply(yp,y)) - multiply(mean(yp), mean(y))
+    term_4_y[turn-1] = mean(multiply(yp,yp)) - multiply(mean(yp),mean(yp))
 
-    det_y = dot(term_1_y, term_4_y) - dot(term_3_y, term_2_y)
-    em_y[turn-1] = sqrt(abs(det_y))
+    det_y[turn-1] = multiply(term_1_y[turn-1], term_4_y[turn-1]) - multiply(term_3_y[turn-1], term_2_y[turn-1])
+    em_y[turn-1] = sqrt(abs(det_y[turn-1]))*beta_rel*gamma_rel
+
+    t[turn-1] = turn
 
 print em_x
 print em_y
+
+# ------------------------------------------------------------------------------
+# Plot characteristics
+# ------------------------------------------------------------------------------
+DPI = 300
+textwidth = 4
+rc('font',**{'family':'serif','serif':['Computer Modern Roman'], 'size':10})
+rc('text', usetex=True)
+rcParams['figure.figsize']=textwidth, textwidth/1.618
+
+# ------------------------------------------------------------------------------
+# Plot the emittance
+# ------------------------------------------------------------------------------
+plt.plot(t, em_x, 'b', label='X')
+plt.plot(t, em_y, 'r', label='Y')
+plt.grid(b=None, which='major')
+plt.legend(loc='lower right')
+plt.xlabel('Turns')
+plt.ylabel(r'$\epsilon_n$ [m]')
+plt.ticklabel_format(style='sci',axis='y',scilimits=(0,0))
+plt.tight_layout()
+plt.savefig('emittance_vs_turns.png', dpi=DPI)
+plt.close()
+
+# ------------------------------------------------------------------------------
+# Plot the mean
+# ------------------------------------------------------------------------------
+plt.plot(t, mean_x, 'b', label='X')
+plt.plot(t, mean_y, 'r', label='Y')
+plt.grid(b=None, which='major')
+plt.legend(loc='upper left')
+plt.xlabel('Turns')
+plt.ylabel('Mean [m]')
+plt.ticklabel_format(style='sci',axis='y',scilimits=(0,0))
+plt.savefig('mean_vs_turns.png', dpi=DPI)
+plt.close()
