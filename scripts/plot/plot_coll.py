f4c02f974213fbf516d17d320a1581985c247cc1
Andrea <andrea.stmaria@gmail.com>
2015-09-15 18:19:46 +0200

    Improvement of efficiency - Reading the input files line by line using a new function - Simple implementation of the histograms with matplotlib - Addition of the plt.clf() command to clean the figures
    ToDo:
    - Add the gap to the coordinate histogram
    - Change the commas to slashes for the input
    - Research how to input the data just after calling the script to automatize it?

--- scripts/plot/plot_coll.py
+++ scripts/plot/plot_coll.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 import pylab as P
-from numpy import *
+import numpy as np
 from collections import Counter
 from matplotlib import pyplot as plt
 from matplotlib import rc
@@ -11,13 +11,14 @@ from util import *
 # ------------------------------------------------------------------------------
 # Feed the input to the script by command line
 # ------------------------------------------------------------------------------
-infile_1 = raw_input("What's the name of your impacts real file? >> ")
-name = raw_input("What's the name of the collimator? >> ")
-name = '%s'%name
-id_col = raw_input("What's the collimator's ID number? >> ")
-id_col= float(id_col)
-halfgap = raw_input("What's the halfgap? >> ")
-total_particles = raw_input("What's the total number of particles? >> ")
+user_input = raw_input('impacts real, coll id, coll name, orientation, halfgap, # of particles >> ')
+list_input = user_input.split(',')
+infile = list_input[0].strip()
+id_col = float(list_input[1])
+name = '%s' %list_input[2].strip()
+orientation = str(list_input[3]).strip()
+halfgap = float(list_input[4])
+total_particles = list_input[5]
 
 # ------------------------------------------------------------------------------
 # ############################## IMPACTS REAL ##################################
@@ -25,26 +26,20 @@ total_particles = raw_input("What's the total number of particles? >> ")
 # ------------------------------------------------------------------------------
 # Extract losses in the aperture
 # ------------------------------------------------------------------------------
-impacts_data = loadtxt(infile_1)
-
-s = []
-x = []
-y= []
-icoll = []
-for line in impacts_data:
-    s.append(line[2])
-    x.append(line[3])
-    y.append(line[5])
-    icoll.append(line[0])
+icoll = load_data(infile, 0)
+s = load_data(infile, 2)
+x = load_data(infile, 3)
+y = load_data(infile, 5)
 
 x_tct = []
 y_tct = []
-for e1,e2,e3 in zip(icoll, x, y):
+s_tct = []
+for e1,e2,e3,e4 in zip(icoll, x, y, s):
     if e1==id_col:
         x_tct.append(e2)
         y_tct.append(e3)
-print type(id_col)
-
+        s_tct.append(e4)
+        
 # ------------------------------------------------------------------------------
 # Plot characteristics
 # ------------------------------------------------------------------------------
@@ -55,38 +50,56 @@ rc('text', usetex=True)
 rcParams['figure.figsize']=textwidth, textwidth/1.618
 
 # ------------------------------------------------------------------------------
-# Plot the impacts on the collimator
+# Plot the impacts on the collimator in x-y
 # ------------------------------------------------------------------------------
 plt.scatter(x_tct,y_tct, marker=(5,2))
 plt.grid(b=None, which='major')
-# plt.xlim([0, 26658.883])
-# plt.ylim([0, 1])
-plt.xlabel("x [mm]]")
+plt.xlabel("x [mm]")
 plt.ylabel("y [mm]")
-plt.title(name + "hits =" + str(len(x_tct)))
+plt.title(name + ". Hits = " + str(len(x_tct)) + '/' + str(total_particles))
+plt.grid(b=None, which='major')
 plt.subplots_adjust(left=0.16, bottom=0.19, right=0.94, top=0.88)
-plt.savefig(name + '.png', dpi=DPI)
+plt.savefig(name + '_xy.png', dpi=DPI)
+plt.clf()
 
 # ------------------------------------------------------------------------------
-# Halfgap (to be read from collgaps)
+# Plot the impacts on the collimator in y-s
 # ------------------------------------------------------------------------------
-halfgap = float(halfgap)
-halfgap_minus = halfgap -1
-
-# ------------------------------------------------------------------------------
-# Impact parameter
-# ------------------------------------------------------------------------------
-abs_coord = []
-for e1 in y_tct: #or x, depending on your collimator
-        abs_coord.append(abs(e1) - halfgap)
+plt.scatter(s_tct,y_tct, marker=(5,2))
+plt.grid(b=None, which='major')
+plt.xlabel("s [m]")
+plt.ylabel("y [mm]")
+plt.title(name + ". Hits = " + str(len(x_tct)) + '/' + str(total_particles))
+plt.grid(b=None, which='major')
+plt.subplots_adjust(left=0.16, bottom=0.19, right=0.94, top=0.88)
+plt.savefig(name + '_sy.png', dpi=DPI)
+plt.clf()
 
 # ------------------------------------------------------------------------------
 # Plot the impact parameter
-# ------------------------------------------------------------------------------        
-histogram_coll(abs_coord, 700, False, name, halfgap, halfgap_minus, 'Impact parameter')
+# ------------------------------------------------------------------------------
+abs_coord = []
+if orientation=='vertical':
+    for e1 in y_tct: 
+            abs_coord.append(abs(e1) - halfgap)
+
+plt.hist(abs_coord, 100, color='green', alpha=0.8)
+plt.xlabel("Impact parameter (mm)")
+plt.grid(b=None, which='major')
+plt.title(name + ". Hits = " + str(len(x_tct)) + '/' + str(total_particles))
+plt.subplots_adjust(left=0.16, bottom=0.19, right=0.94, top=0.88)
+plt.savefig(name + '_histogram_impact_parameter.png', dpi=DPI)
+plt.clf()
 
 # ------------------------------------------------------------------------------
-# Plot the histogram
-# ------------------------------------------------------------------------------   
-histogram_coll(y_tct, 100, True, name, halfgap, halfgap_minus,'coordinate')
+# Plot the distribution histogram
+# ------------------------------------------------------------------------------
+plt.hist(y_tct, 100, color='green', alpha=0.8)
+plt.xlabel("y (mm)")
+plt.grid(b=None, which='major')
+plt.title(name + ". Hits = " + str(len(x_tct)) + '/' + str(total_particles))
+plt.subplots_adjust(left=0.16, bottom=0.19, right=0.94, top=0.88)
+plt.savefig(name + '_histogram_coordinate.png', dpi=DPI)
+
+
 
