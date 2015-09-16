1d4caf61eecedfc76b752f8d8010c2e6a3ac5af1
Andrea <andrea.stmaria@gmail.com>
2015-09-16 17:47:04 +0200

    Complete remake of the loss maps plotting script - It has been optimized so that it only loops once through impacts real - The histogram functionality of matplolib has been used, instead of doing ti by hand - For a file of 3.4 GB, the script takes 0:01:03.777734

--- scripts/plot/plot_loss_maps.py
+++ scripts/plot/plot_loss_maps.py
@@ -1,77 +1,41 @@
-import os.path
+#!/usr/local/bin/python
+import numpy as np
+from datetime import datetime
 from matplotlib import pyplot as plt
 from matplotlib import rc
 from matplotlib import rcParams
-from numpy import *
-from util import *
 
 # ------------------------------------------------------------------------------
-# Specify the total number of particles simulated
+# Feed the input to the script by command line
 # ------------------------------------------------------------------------------
-total_particles = 3200000
+user_input = raw_input('LPI/impacts_real/coll_summary/coll_positions/total particles >> ')
+list_input = user_input.split('/')
+infile_lpi = list_input[0].strip()
+infile_impacts = list_input[1].strip()
+infile_collsum = list_input[2].strip()
+infile_collpos = list_input[3].strip()
+total_particles = float(list_input[4].strip())
+
+start_time = datetime.now()
 # ------------------------------------------------------------------------------
 # ################################## APERTURE ##################################
-# ------------------------------------------------------------------------------   
 # ------------------------------------------------------------------------------
-# Extract losses in the aperture
-# ------------------------------------------------------------------------------
-aperture_data = loadtxt("aperture.dat")
-positions_LPI = []
-
-for line in aperture_data:
-    positions_LPI.append(line[2])
-
-# ------------------------------------------------------------------------------
-# Extract the unique values of the position
-# ------------------------------------------------------------------------------
-set_unique_positions_LPI = set(positions_LPI)
-list_unique_positions_LPI = list(set_unique_positions_LPI)
-
-# ------------------------------------------------------------------------------
-# Extract from the list how many times that position appears
-# ------------------------------------------------------------------------------
-occurrences_LPI = [float(positions_LPI.count(position)) for position in list_unique_positions_LPI] 
-
-# ------------------------------------------------------------------------------
-# Order the tuples by amount of particles lost at that location
-# ------------------------------------------------------------------------------
-sorted_LPI = sorted(zip(list_unique_positions_LPI, occurrences_LPI), key=lambda t:t[1])
-
-# ------------------------------------------------------------------------------
-# Transfer the information to a file
-# ------------------------------------------------------------------------------    
-filename_ap = "losses_aperture.txt"
-if os.path.exists(filename_ap)==True:
-    os.remove(filename_ap)
-    
-g = open(filename_ap, "a")
-for e1, e2 in sorted_LPI:
-    print >> g, e1, e2
+g = open(infile_lpi, 'r')
+pos_lpi = []
+for line in g.xreadlines():
+    columns = line.strip('\n').split()
+    if columns[0] == '#' or columns[0] == '@' or columns[0] == '*' or columns[0] == '$' or columns[0] == '%' or columns[0] == '%1=s' or columns[0] == '%Ind':
+        continue
+    pos_lpi.append(float(columns[2]))
 g.close()
-
-# ------------------------------------------------------------------------------
-# Convert the data to numpy arrays and normalize it
-# ------------------------------------------------------------------------------   
-x_aperture = asarray(list_unique_positions_LPI)
-y_aperture = asarray(occurrences_LPI)/total_particles
 
 # ------------------------------------------------------------------------------
 # ############################### COLLIMATION ##################################
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
-# Extract ID from impacts_real.dat
-# ------------------------------------------------------------------------------
-collimation_data = loadtxt("imp_real.dat")
-
-id_impreal = []
-
-for line in collimation_data:
-    id_impreal.append(line[0])
-
-# ------------------------------------------------------------------------------
 # Extract ID and name from coll_summary.dat
 # ------------------------------------------------------------------------------
-collsum_data = loadtxt("coll_sum.dat", str)
+collsum_data = np.loadtxt("coll_sum.dat", str)
 
 name_collsum = []
 id_collsum = []
@@ -83,7 +47,7 @@ for line in collsum_data:
 # ------------------------------------------------------------------------------
 # Extract name and position from CollPositions
 # ------------------------------------------------------------------------------
-collpos_data = loadtxt("CollPositions.b1.dat", str, "%Ind")
+collpos_data = np.loadtxt("CollPositions.b1.dat", str, "%Ind")
 
 name_collpos = []
 position_collpos = []
@@ -91,11 +55,6 @@ position_collpos = []
 for line in collpos_data:
     name_collpos.append(line[1])
     position_collpos.append(float(line[2]))
-
-# ------------------------------------------------------------------------------
-# Extract from the list how many times the collimator ID appears
-# ------------------------------------------------------------------------------
-occurrences_impreal = [float(id_impreal.count(i)) for i in id_collsum]
 
 # ------------------------------------------------------------------------------
 # Associate name with ID via coll_summary, then extract correct position
@@ -113,26 +72,28 @@ for e1, e2 in zip(name_collpos, position_collpos):
             position.append(e2)
 
 # ------------------------------------------------------------------------------
-# Transfer the information to a file
-# ------------------------------------------------------------------------------    
-filename_coll = "losses_collimation.txt"
-if os.path.exists(filename_coll)==True:
-    os.remove(filename_coll)
-    
-f = open(filename_coll, "a")
-for e1, e2, e3, e4 in zip(id_collsum, name, position, occurrences_impreal):
-    print >> f, e1, e2, e3, e4
+# Create a frigging dictionary with the correct sh*t
+# ------------------------------------------------------------------------------
+my_tuple = zip(id_collsum, position)
+d = {}
+for k, v in my_tuple:
+    d[k] = v
+
+# ------------------------------------------------------------------------------
+# Loop >ONCE< through impacts real to extract the coll ID's (no way around it)
+# ------------------------------------------------------------------------------
+f = open(infile_impacts, 'r')
+pos = []
+for line in f.xreadlines():
+    columns = line.strip('\n').split()
+    if columns[0] == '#' or columns[0] == '@' or columns[0] == '*' or columns[0] == '$' or columns[0] == '%' or columns[0] == '%1=s' or columns[0] == '%Ind':
+        continue
+    pos.append(d[int(columns[0])])
 f.close()
 
 # ------------------------------------------------------------------------------
-# Convert the data to numpy arrays and normalize it
-# ------------------------------------------------------------------------------   
-x_coll = asarray(position)
-y_coll = asarray(occurrences_impreal)/total_particles
-
-# ------------------------------------------------------------------------------
 # ################################### PLOT #####################################
-# ------------------------------------------------------------------------------   
+# ------------------------------------------------------------------------------ 
 # ------------------------------------------------------------------------------
 # Plot characteristics
 # ------------------------------------------------------------------------------
@@ -142,26 +103,22 @@ rc('font',**{'family':'serif','serif':['Computer Modern Roman'], 'size':10})
 rc('text', usetex=True)
 rcParams['figure.figsize']=textwidth, textwidth/1.618
 
-# ------------------------------------------------------------------------------
-# Plot bars
-# ------------------------------------------------------------------------------
-plt.bar(x_aperture, y_aperture, color='g', label='Aperture', log=True, width=1, align='center', edgecolor='g')
-plt.bar(x_coll, y_coll, color='r', label='Collimators', log=True, width=1, align='center', edgecolor='r')
+pos_coll = np.asarray(pos)
+pos_ap = np.asarray(pos_lpi)
 
-# ------------------------------------------------------------------------------
-# Annotate the IP names
-# ------------------------------------------------------------------------------
-ip_name = ['IP1', 'IP2', 'IR3', 'IR4', 'IP5', 'IR6', 'IR7', 'IP8']
-ip_position = [800, 3332.4, 6664.721, 9997, 13329.28, 16661.7, 20000, 23315.4]
+weights_coll = np.ones_like(pos_coll)/total_particles
+weights_ap = np.ones_like(pos_ap)/total_particles
 
-for i in range(0,8):
-    plt.annotate(ip_name[i], xy=(1, 10**-4), xytext=(ip_position[i], 10**-4), weight='bold', va='bottom', ha='center', size=8)
-
+plt.hist(pos, 500, color='black', alpha=0.8, linewidth=0.1, weights=weights_coll, log=True, label='Collimation')
+plt.hist(pos_lpi, 500, color='green', alpha=0.8, linewidth=0.1, weights=weights_ap, log=True, label='Aperture')
+plt.xlabel("s (m)")
+plt.ylabel("Particles lost per 10 cm")
 plt.grid(b=None, which='major')
-plt.xlim([0, 26658.883])
-plt.ylim([0, 1])
-plt.xlabel("s [m]")
-plt.ylabel("Fraction of particles lost")
-plt.legend(loc = 'upper left',fontsize=10)
-plt.savefig('loss_map.png', dpi=DPI)
-# plt.show()
+plt.title('Loss map')
+plt.legend(loc='upper right', prop={'size':6})
+plt.subplots_adjust(left=0.16, bottom=0.19, right=0.94, top=0.88)
+plt.savefig('loss_maps.png', dpi=DPI)
+plt.clf()
+
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
