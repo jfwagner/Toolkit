babd5e9ca0fb15e65357dc8ab76eac8a0fa225c1
Andrea <andrea.stmaria@gmail.com>
2015-09-16 15:14:38 +0200

    Finalization of plot_coll.py - Optimization of the execution time by reducing the for loops from 3 to 1, in the load_data_coll function - Changing commas for slashes for the separators of the input string - Implementing the treatment for horizontal coordinates

--- scripts/plot/plot_coll.py
+++ scripts/plot/plot_coll.py
@@ -11,8 +11,8 @@ from util import *
 # ------------------------------------------------------------------------------
 # Feed the input to the script by command line
 # ------------------------------------------------------------------------------
-user_input = raw_input('impacts real, coll id, coll name, orientation, halfgap, # of particles >> ')
-list_input = user_input.split(',')
+user_input = raw_input('impacts real/coll id/coll name/orientation/halfgap (mm)/# of particles >> ')
+list_input = user_input.split('/')
 infile = list_input[0].strip()
 id_col = float(list_input[1])
 name = '%s' %list_input[2].strip()
@@ -28,12 +28,8 @@ total_particles = list_input[5]
 # ------------------------------------------------------------------------------
 from datetime import datetime
 start_time = datetime.now()
-s_tct = load_data_coll(infile, 2, list_input[1])
-x_tct = load_data_coll(infile, 3, list_input[1])
-y_tct = load_data_coll(infile, 5, list_input[1])
-end_time = datetime.now()
-print('Duration: {}'.format(end_time - start_time))
-        
+s_tct, x_tct, y_tct = load_data_coll(infile, list_input[1])
+
 # ------------------------------------------------------------------------------
 # Plot characteristics
 # ------------------------------------------------------------------------------
@@ -57,42 +53,51 @@ plt.savefig(name + '_xy.png', dpi=DPI)
 plt.clf()
 
 # ------------------------------------------------------------------------------
-# Plot the impacts on the collimator in y-s
+# Coordinate dependent plots
 # ------------------------------------------------------------------------------
-plt.scatter(s_tct,y_tct, marker=(5,2))
+if orientation=='vertical':
+    coord_tct = y_tct
+    my_label = "y [mm]"
+elif orientation=='horizontal':
+    coord_tct = x_tct
+    my_label = "x [mm]"
+    
+# ------------------------------------------------------------------------------
+# Impacts on the collimator in coord-s
+# ------------------------------------------------------------------------------
+plt.scatter(s_tct,coord_tct, marker=(5,2))
 plt.grid(b=None, which='major')
 plt.xlabel("s [m]")
-plt.ylabel("y [mm]")
+plt.ylabel(my_label)
 plt.title(name + ". Hits = " + str(len(x_tct)) + '/' + str(total_particles))
 plt.grid(b=None, which='major')
 plt.subplots_adjust(left=0.16, bottom=0.19, right=0.94, top=0.88)
-plt.savefig(name + '_sy.png', dpi=DPI)
+plt.savefig(name + '_scoord.png', dpi=DPI)
 plt.clf()
 
 # ------------------------------------------------------------------------------
-# Plot the distribution histogram
+# Histogram of coord
 # ------------------------------------------------------------------------------
-n, bins, patches = plt.hist(y_tct, 100, color='green', alpha=0.8, linewidth=0.1)
+n, bins, patches = plt.hist(coord_tct, 100, color='green', alpha=0.8, linewidth=0.1)
 plt.bar(halfgap, max(n), color='blue', log=False, width=0.0005, align='center', edgecolor='blue', linewidth=0.1)
 plt.bar(-1*halfgap, max(n), color='blue', log=False, width=0.0005, align='center', edgecolor='blue', linewidth=0.1)
 x_halfgap = linspace(-1*halfgap, halfgap)
 plt.fill_between(x_halfgap, max(n), facecolor='blue', alpha=0.2, label='Gap', linewidth=0.1)
-plt.xlabel("y (mm)")
+plt.xlabel(my_label)
 plt.grid(b=None, which='major')
 plt.title(name + ". Hits = " + str(len(x_tct)) + '/' + str(total_particles))
 plt.legend(loc='upper right', prop={'size':6})
 plt.subplots_adjust(left=0.16, bottom=0.19, right=0.94, top=0.88)
-plt.savefig(name + '_histogram_coordinate.png', dpi=DPI)
+plt.savefig(name + '_histogram_coord.png', dpi=DPI)
 plt.clf()
 
 # ------------------------------------------------------------------------------
-# Plot the impact parameter
+# Histogram of the impact parameter
 # ------------------------------------------------------------------------------
 abs_coord = []
-if orientation=='vertical':
-    for e1 in y_tct: 
-            abs_coord.append(abs(e1) - halfgap)
-
+for e1 in coord_tct: 
+    abs_coord.append(abs(e1) - halfgap)
+    
 plt.hist(abs_coord, 100, color='green', alpha=0.8, linewidth=0.1)
 plt.xlabel("Impact parameter (mm)")
 plt.grid(b=None, which='major')
@@ -101,7 +106,6 @@ plt.subplots_adjust(left=0.16, bottom=0.19, right=0.94, top=0.88)
 plt.savefig(name + '_histogram_impact_parameter.png', dpi=DPI)
 plt.clf()
 
-
-
-
-
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
+        
