806c95578121731e2c634636cc8eb4463d0d1d1c
Andrea <andrea.stmaria@gmail.com>
2016-01-20 16:25:46 +0100

    Improvement of script - Input arguments by command line, don't have to be ordered - Directly plotting from dictionary - Adding more user options: plot around IP1 - Survey files are optional - Position of elements self adjust - Only two plotting options available: ORBIT or BETA

--- scripts/plot/plot_beams.py
+++ scripts/plot/plot_beams.py
@@ -2,6 +2,7 @@
 # ------------------------------------------------------------------------------
 # A script to read TFS files
 # ------------------------------------------------------------------------------
+import argparse
 import re
 import sys
 import numpy as np
@@ -13,211 +14,155 @@ from util import get_madx_columns
 from util import plot_elem
 
 # ------------------------------------------------------------------------------
-# Feed the input to the script by command line
-# ------------------------------------------------------------------------------
-infile_b1_twiss = sys.argv[1]
-infile_b2_twiss = sys.argv[2]
-infile_b1_survey = sys.argv[3]
-infile_b2_survey = sys.argv[4]
-headers = sys.argv[5:]
-
 # Constants
+# ------------------------------------------------------------------------------
 gamma_rel = 7460.52280875
 beta_rel = 0.999999991017
-emittance = 2.5e-6
 
-# TWISS, BEAM 1
-#-------------------------------------------------------------------------------
-# Get the twiss data as a dictionary
-for item in headers:
-    data_dict = get_madx_columns(infile_b1_twiss, item)
-    print data_dict[item]
-    
-    # data_dict[item]
-    # print  '%s' %(item + '_twiss_b1')
-#     var = data_dict[item]
+# ------------------------------------------------------------------------------
+# Feed the input to the script by command line
+# ------------------------------------------------------------------------------
+__author__ = 'Andrea Santamaria Garcia'
+ 
+parser = argparse.ArgumentParser(description='This is a script to plot data from tfs files.')
+parser.add_argument('-t_b1','--twiss_b1', help='Twiss Beam 1 file',required=True)
+parser.add_argument('-t_b2','--twiss_b2',help='Twiss Beam 2 file', required=True)
+parser.add_argument('-s_b1','--survey_b1', help='Survey Beam 1 file',required=False)
+parser.add_argument('-s_b2','--survey_b2',help='Survey Beam 2 file', required=False)
+parser.add_argument('-em','--emittance', help='Emittance value in [m]',required=True, type=float)
+parser.add_argument('-p','--plot',help='Plot type', required=True)
+parser.add_argument('-ip1','--plot_ip1',help='Plotting around IP1', required=True)
+args = parser.parse_args()
+ 
+## show values ##
+print " "
+print "********************************************"
+print "PLOTTING PARAMETERS"
+print "********************************************"
+print ("Twiss file for Beam 1: %s" % args.twiss_b1 )
+print ("Twiss file for Beam 2: %s" % args.twiss_b2 )
+print ("Survey file for Beam 1: %s" % args.survey_b1 )
+print ("Survey file for Beam 2: %s" % args.survey_b2 )
+print ("Emittance [m]: %s" % args.emittance )
+print ("Plot type: %s" % args.plot )
+print ("Plotting around IP1: %s" % args.plot_ip1 )
+print " "
 
-# print S_twiss_b1
-    
+## put the data in a dictionary ##
+twiss_b1 = get_madx_columns(args.twiss_b1, 'S', 'X', 'Y', 'BETX', 'BETY', 'NAME', 'L')
+twiss_b2 = get_madx_columns(args.twiss_b2, 'S', 'X', 'Y', 'BETX', 'BETY', 'NAME', 'L')
 
-# Extract the data from the dictionary to lists for each column
-# s_twiss_b1 = twiss_b1['S']
-# l_twiss_b1 = twiss_b1['L']
-# name_twiss_b1 = twiss_b1['NAME']
-# x_twiss_b1 = twiss_b1['X']
-# y_twiss_b1 = twiss_b1['Y']
-# beta_x_twiss_b1 = twiss_b1['BETX']
-# beta_y_twiss_b1 = twiss_b1['BETY']
-
-# # TWISS, BEAM 2
-# #-------------------------------------------------------------------------------
-# # Get the twiss data as a dictionary
-# twiss_b2 = get_madx_columns(infile_b2_twiss, 'S', 'L', 'BETX', 'BETY', 'X', 'Y', 'NAME')
-
-# # Extract the data from the dictionary to lists for each column
-# s_twiss_b2 = twiss_b2['S']
-# l_twiss_b2 = twiss_b2['L']
-# name_twiss_b2 = twiss_b2['NAME']
-# x_twiss_b2 = twiss_b2['X']
-# y_twiss_b2 = twiss_b2['Y']
-# beta_x_twiss_b2 = twiss_b2['BETX']
-# beta_y_twiss_b2 = twiss_b2['BETY']
-
-# # SURVEY, BEAM 1
-# #-------------------------------------------------------------------------------
-# # Get the survey data as a dictionary
-# survey_b1 = get_madx_columns(infile_b1_survey, 'S', 'L', 'BETX', 'BETY', 'X', 'Y', 'NAME')
-
-# # Extract the data from the dictionary to lists for each column
-# s_survey_b1 = survey_b1['S']
-# l_survey_b1 = survey_b1['L']
-# name_survey_b1 = survey_b1['NAME']
-# x_survey_b1 = survey_b1['X']
-# y_survey_b1 = survey_b1['Y']
-# beta_x_survey_b1 = survey_b1['BETX']
-# beta_y_survey_b1 = survey_b1['BETY']
-
-# # SURVEY, BEAM 2
-# #-------------------------------------------------------------------------------
-# # Get the survey data as a dictionary
-# survey_b2 = get_madx_columns(infile_b2_survey, 'S', 'L', 'BETX', 'BETY', 'X', 'Y', 'NAME')
-
-# # Extract the data from the dictionary to lists for each column
-# s_survey_b2 = survey_b2['S']
-# l_survey_b2 = survey_b2['L']
-# name_survey_b2 = survey_b2['NAME']
-# x_survey_b2 = survey_b2['X']
-# y_survey_b2 = survey_b2['Y']
-# beta_x_survey_b2 = survey_b2['BETX']
-# beta_y_survey_b2 = survey_b2['BETY']
+## plot characteristics ##
+DPI = 300
+textwidth = 6
+font_spec = {"font.family": "serif", # use as default font
+             "font.serif": ["New Century Schoolbook"], # custom serif font
+             "font.sans-serif": ["helvetica"], # custom sans-serif font
+             "font.size":14,
+             "font.weight":"bold",
+            }
+rc('text', usetex=True)
+rc('text.latex', preamble=r'\usepackage{cmbright}')
+rcParams['figure.figsize']=textwidth, textwidth/1.618
+rcParams.update(font_spec)
 
 
-# # ------------------------------------------------------------------------------
-# # PLOTTING
-# # ------------------------------------------------------------------------------
-# # Plot characteristics
-# DPI = 300
-# textwidth = 6
-# font_spec = {"font.family": "serif", # use as default font
-#              "font.serif": ["New Century Schoolbook"], # custom serif font
-#              "font.sans-serif": ["helvetica"], # custom sans-serif font
-#              "font.size":14,
-#              "font.weight":"bold",
-#             }
-# rc('text', usetex=True)
-# rc('text.latex', preamble=r'\usepackage{cmbright}')
-# rcParams['figure.figsize']=textwidth, textwidth/1.618
-# rcParams.update(font_spec)
+## start the correct plot loop ##
+if args.plot == "ORBIT":
+    if args.plot_ip1 == "no":
+        plt.plot(twiss_b1['S'], twiss_b1['Y'] ,color='blue', label='Beam 1')
+        plt.plot(twiss_b2['S'], twiss_b2['Y'] , color='red', label='Beam 2')
+        ## plot the elements ##
+        height = max(twiss_b1['Y'])/5
+        bottom = max(twiss_b1['Y']) + max(twiss_b1['Y'])/6
+        plot_elem('red', height, bottom, twiss_b1['NAME'], twiss_b1['S'], twiss_b1['L'],  'MQXFA', 'MQXFB') # Triplet: Q1, Q3, Q2
+        plot_elem('blue', height, bottom, twiss_b1['NAME'], twiss_b1['S'], twiss_b1['L'],  'MBX', 'MBRC', 'MBRS', 'MBRB', 'MB') # Dipoles: D1, D2, D3, D4
+        plot_elem('orange', height, bottom, twiss_b1['NAME'], twiss_b1['S'], twiss_b1['L'],  'TAXS', 'TAXN') # Passive protectors: TAS, TAN
+        plot_elem('black', height, bottom, twiss_b1['NAME'], twiss_b1['S'], twiss_b1['L'],  'TCT') # Tertiary collimators
+        plot_elem('green', height, bottom, twiss_b1['NAME'], twiss_b1['S'], twiss_b1['L'],  'ACF') # Crab cavities
+        plt.xlabel("s (m)")
+        plt.ylabel("Vertical orbit (m)")
+        plt.xlim([-200,200])
+        plt.grid(b=None, which='major')
+        plt.legend(loc='lower right', prop={'size':9})
+        plt.ticklabel_format(style='sci',axis='y',scilimits=(0,0))
+        plt.subplots_adjust(left=0.15, bottom=0.15, right=0.94, top=0.90)
+        plt.savefig('orbit_y.png', dpi=DPI)
+        plt.clf()
+    elif args.plot_ip1 == "yes":
+        s_b1, y_b1 = get_ip1(twiss_b1['S'], twiss_b1['Y'])
+        s_b2, y_b2 = get_ip1(twiss_b2['S'], twiss_b2['Y'])
+        plt.plot(s_b1, y_b1, color='blue', label='Beam 1')
+        plt.plot(s_b2, y_b2, color='red', label='Beam 2')
+        ## plot the elements ##
+        height = max(twiss_b1['Y'])/5
+        bottom = max(twiss_b1['Y']) + max(twiss_b1['Y'])/6
+        s_temp, name  = get_ip1(twiss_b1['S'], twiss_b1['NAME'])
+        s, l = get_ip1(twiss_b1['S'], twiss_b1['L'])
+        plot_elem('red', height, bottom, name, s, l,  'MQXFA', 'MQXFB') # Triplet: Q1, Q3, Q2
+        plot_elem('blue', height, bottom, name, s, l,  'MBX', 'MBRC', 'MBRS', 'MBRB', 'MB') # Dipoles: D1, D2, D3, D4
+        plot_elem('orange', height, bottom, name, s, l,  'TAXS', 'TAXN') # Passive protectors: TAS, TAN
+        plot_elem('black', height, bottom, name, s, l,  'TCT') # Tertiary collimators
+        plot_elem('green', height, bottom, name, s, l,  'ACF') # Crab cavities
+        plt.xlabel("s (m)")
+        plt.ylabel("Vertical orbit (m)")
+        plt.xlim([-200,200])
+        plt.grid(b=None, which='major')
+        plt.legend(loc='lower right', prop={'size':9})
+        plt.ticklabel_format(style='sci',axis='y',scilimits=(0,0))
+        plt.subplots_adjust(left=0.15, bottom=0.15, right=0.94, top=0.90)
+        plt.savefig('orbit_y.png', dpi=DPI)
+        plt.clf()
+    else:
+        print "Input for plottinga round IP1 option should be yes or no"
+   
+elif args.plot == "BETA":
+    if args.plot_ip1 == "no":
+        plt.plot(twiss_b1['S'], twiss_b1['BETY'] ,color='blue', label='Beta Y')
+        plt.plot(twiss_b1['S'], twiss_b1['BETX'] , color='green', label='Beta X')
+        ## plot the elements ##
+        height = max(twiss_b1['BETY'])/5
+        bottom = max(twiss_b1['BETY']) + max(twiss_b1['BETY'])/6
+        plot_elem('red', height, bottom, twiss_b1['NAME'], twiss_b1['S'], twiss_b1['L'],  'MQXFA', 'MQXFB') # Triplet: Q1, Q3, Q2
+        plot_elem('blue', height, bottom, twiss_b1['NAME'], twiss_b1['S'], twiss_b1['L'],  'MBX', 'MBRC', 'MBRS', 'MBRB', 'MB') # Dipoles: D1, D2, D3, D4
+        plot_elem('orange', height, bottom, twiss_b1['NAME'], twiss_b1['S'], twiss_b1['L'],  'TAXS', 'TAXN') # Passive protectors: TAS, TAN
+        plot_elem('black', height, bottom, twiss_b1['NAME'], twiss_b1['S'], twiss_b1['L'],  'TCT') # Tertiary collimators
+        plot_elem('green', height, bottom, twiss_b1['NAME'], twiss_b1['S'], twiss_b1['L'],  'ACF') # Crab cavities
+        plt.xlabel("s (m)")
+        plt.ylabel("Beta functions (m)")
+        plt.xlim([-200,200])
+        plt.grid(b=None, which='major')
+        plt.legend(loc='lower right', prop={'size':9})
+        plt.ticklabel_format(style='sci',axis='y',scilimits=(0,0))
+        plt.subplots_adjust(left=0.15, bottom=0.15, right=0.94, top=0.90)
+        plt.savefig('beta_functions.png', dpi=DPI)
+        plt.clf()
+    elif args.plot_ip1 == "yes":
+        s_b1_temp, betay_b1 = get_ip1(twiss_b1['S'], twiss_b1['BETY'])
+        s_b1, betax_b1 = get_ip1(twiss_b1['S'], twiss_b1['BETX'])
+        plt.plot(s_b1, betay_b1, color='blue', label='Beta Y')
+        plt.plot(s_b1, betax_b1, color='green', label='Beta X')
+        ## plot the elements ##
+        height = max(twiss_b1['BETY'])/5
+        bottom = max(twiss_b1['BETY']) + max(twiss_b1['BETY'])/6
+        s_temp, name  = get_ip1(twiss_b1['S'], twiss_b1['NAME'])
+        s, l = get_ip1(twiss_b1['S'], twiss_b1['L'])
+        plot_elem('red', height, bottom, name, s, l,  'MQXFA', 'MQXFB') # Triplet: Q1, Q3, Q2
+        plot_elem('blue', height, bottom, name, s, l,  'MBX', 'MBRC', 'MBRS', 'MBRB', 'MB') # Dipoles: D1, D2, D3, D4
+        plot_elem('orange', height, bottom, name, s, l,  'TAXS', 'TAXN') # Passive protectors: TAS, TAN
+        plot_elem('black', height, bottom, name, s, l,  'TCT') # Tertiary collimators
+        plot_elem('green', height, bottom, name, s, l,  'ACF') # Crab cavities
+        plt.xlabel("s (m)")
+        plt.ylabel("Beta functions (m)")
+        plt.xlim([-200,200])
+        plt.grid(b=None, which='major')
+        plt.legend(loc='lower right', prop={'size':9})
+        plt.ticklabel_format(style='sci',axis='y',scilimits=(0,0))
+        plt.subplots_adjust(left=0.15, bottom=0.15, right=0.94, top=0.90)
+        plt.savefig('beta_functions.png', dpi=DPI)
+        plt.clf()
+    else:
+        print "Input for plottinga round IP1 option should be yes or no"
+else:
+    print "Sorry, that plotting option does not exist"
 
-# # Plot the vertical orbit
-# plt.plot(s_twiss_b1, y_twiss_b1,color='blue', label='Beam 1')
-# plt.plot(s_twiss_b2, y_twiss_b2,color='red', label='Beam 2')
-# plt.xlabel("s (m)")
-# plt.ylabel("Vertical orbit (m)")
-# plt.xlim([-200,200])
-# plt.grid(b=None, which='major')
-# plt.legend(loc='lower right', prop={'size':9})
-# plt.ticklabel_format(style='sci',axis='y',scilimits=(0,0))
-# plt.subplots_adjust(left=0.12, bottom=0.15, right=0.97, top=0.91)
-# plt.savefig('orbit_y.png', dpi=DPI)
-# plt.clf()
-
-
-# # Plot the beta functions
-# plt.plot(sx_b1_twiss, betax_b1_twiss, label='Beta x')
-# plt.plot(sy_b1_twiss, betay_b1_twiss, label='Beta y')
-# plt.xlabel("s (m)")
-# plt.ylabel("Beta functions (m)")
-# plt.xlim([-200,200])
-# plt.ylim([0,3e4])
-# plt.grid(b=None, which='major')
-# plt.legend(loc='lower right', prop={'size':9})
-# plt.ticklabel_format(style='sci',axis='y',scilimits=(0,0))
-
-# # Plot the elements
-# height = 3e3
-# bottom = 2.4e4
-# plot_elem('red', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'MQXFA', 'MQXFB') # Triplet: Q1, Q3, Q2
-# plot_elem('blue', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'MBX', 'MBRC', 'MBRS', 'MBRB', 'MB') # Dipoles: D1, D2, D3, D4
-# plot_elem('orange', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'TAXS', 'TAXN') # Passive protectors: TAS, TAN
-# plot_elem('black', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'TCT') # Tertiary collimators
-# plot_elem('green', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'ACF') # CRab cavities
-
-# # Save the plot
-# plt.subplots_adjust(left=0.12, bottom=0.15, right=0.97, top=0.91)
-# plt.savefig('beta_functions.png', dpi=DPI)
-# plt.clf()
-
-# # Plot the beams
-# # BEAM 1
-# # Y
-# s_b1_twiss_array = np.asarray(s_b1_twiss)
-# y_b1_twiss_array = np.asarray(y_b1_twiss)
-# y_b1_survey_array = np.asarray(y_b1_survey)
-# betay_b1_twiss_array = np.asarray(betay_b1_twiss)
-# emy_b1_twiss_array = np.asarray(emy_b1_twiss)
-# sigy_b1_twiss_array = np.asarray(sigy_b1_twiss)
-# sigma_y_b1 = np.sqrt(np.dot(emy_b1_twiss_array,betay_b1_twiss_array))
-# # X
-# x_b1_twiss_array = np.asarray(x_b1_twiss)
-# x_b1_survey_array = np.asarray(x_b1_survey)
-# betax_b1_twiss_array = np.asarray(betax_b1_twiss)
-# emx_b1_twiss_array = np.asarray(emx_b1_twiss)
-# sigx_b1_twiss_array = np.asarray(sigx_b1_twiss)
-# sigma_x_b1 = np.sqrt(np.dot(emx_b1_twiss_array,betax_b1_twiss_array))
-# # BEAM 2
-# # Y
-# s_b2_twiss_array = np.asarray(s_b2_twiss)
-# y_b2_twiss_array = np.asarray(y_b2_twiss)
-# y_b2_survey_array = np.asarray(y_b2_survey)
-# betay_b2_twiss_array = np.asarray(betay_b2_twiss)
-# emy_b2_twiss_array = np.asarray(emy_b2_twiss)
-# sigy_b2_twiss_array = np.asarray(sigy_b2_twiss)
-# sigma_y_b2 = np.sqrt(np.dot(emy_b2_twiss_array,betay_b2_twiss_array))
-# # X
-# x_b2_twiss_array = np.asarray(x_b2_twiss)
-# x_b2_survey_array = np.asarray(x_b2_survey)
-# betax_b2_twiss_array = np.asarray(betax_b2_twiss)
-# emx_b2_twiss_array = np.asarray(emx_b2_twiss)
-# sigx_b2_twiss_array = np.asarray(sigx_b2_twiss)
-# sigma_x_b2 = np.sqrt(np.dot(emx_b2_twiss_array,betax_b2_twiss_array))
-
-# # VERTICAL ORBIT
-# plt.plot(s_b1_twiss_array, y_b1_twiss_array, color='blue', label='Beam 1')
-# plt.plot(s_b2_twiss_array, y_b2_twiss_array, color='red', label='Beam 2')
-# plt.xlabel("s (m)")
-# plt.ylabel("Vertical orbit (m)")
-# plt.xlim([-200,200])
-# plt.grid(b=None, which='major')
-# plt.legend(loc='lower right', prop={'size':9})
-# plt.ticklabel_format(style='sci',axis='y',scilimits=(0,0))
-# height = 0.01
-# bottom = 0.03
-# plot_elem('red', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'MQXFA', 'MQXFB') # Triplet: Q1, Q3, Q2
-# plot_elem('blue', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'MBX', 'MBRC', 'MBRS', 'MBRB', 'MB') # Dipoles: D1, D2, D3, D4
-# plot_elem('orange', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'TAXS', 'TAXN') # Passive protectors: TAS, TAN
-# plot_elem('black', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'TCT') # Tertiary collimators
-# plot_elem('green', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'ACF') # CRab cavities
-# plt.subplots_adjust(left=0.12, bottom=0.15, right=0.97, top=0.91)
-# plt.savefig('orbit_y.png', dpi=DPI)
-# plt.clf()
-
-# HORIZONTAL ORBIT
-# plt.plot(s_b1_twiss_array, x_b1_twiss_array, color='blue', label='Beam 1')
-# plt.plot(s_b2_twiss_array, x_b2_twiss_array, color='red', label='Beam 2')
-# plt.xlabel("s (m)")
-# plt.ylabel("Horizontal orbit (m)")
-# plt.xlim([-200,200])
-# plt.grid(b=None, which='major')
-# plt.legend(loc='lower right', prop={'size':9})
-# plt.ticklabel_format(style='sci',axis='y',scilimits=(0,0))
-# height = 0.01
-# bottom = 0.03
-# plot_elem('red', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'MQXFA', 'MQXFB') # Triplet: Q1, Q3, Q2
-# plot_elem('blue', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'MBX', 'MBRC', 'MBRS', 'MBRB', 'MB') # Dipoles: D1, D2, D3, D4
-# plot_elem('orange', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'TAXS', 'TAXN') # Passive protectors: TAS, TAN
-# plot_elem('black', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'TCT') # Tertiary collimators
-# plot_elem('green', height, bottom, name_b1_twiss, s_b1_twiss, l_b1_twiss,  'ACF') # CRab cavities
-# plt.subplots_adjust(left=0.12, bottom=0.15, right=0.97, top=0.91)
-# plt.savefig('orbit_x.png', dpi=DPI)
-# plt.clf()
