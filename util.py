2453db51d8c41c1a9abd83dced8bc45ca064d28d
Andrea <andrea.stmaria@gmail.com>
2016-03-09 17:52:32 +0100

    Addition of get_bucket function

--- util.py
+++ util.py
@@ -131,6 +131,45 @@ def get_ip1(x, y):
         y.append(e2)
     return x,y
 
+def get_bucket():
+    """ Returns the data needed to plot the RF bucket of LHC.
+    Adapted from Kyrre Sjobak.
+    Usage: plt.contour(PHIp * 0.1, DELTA_E, H, 40, linewidths=0.3, cmap='terrain_r')
+    """
+    mp = 0.938272046e9                 # proton mass, eV/c^2
+    e = 1.60217657e-19                 # C, electron charge
+    c = 2.99792485e8                   # m/s, speed of light
+    h = 35640                          # RF harmonic number
+    omegaRF = 400.8e6 * np.pi * 2      # Hz, omegaRF = h*omega0
+    omega0 = omegaRF / h
+    slip = 3.467e-4                    # Slip factor @ collission
+    V = 16e6                           # V, RF voltage @ collissions
+    beta = 1.0                         # Relativistic beta
+    phiS = 0.0                         # Radians, synchronous RF phase
+    E0 = 7e12                          # Beam energy, eV
+    p0 = np.sqrt(E0 ** 2 - mp ** 2)    # Beam momentum, eV/c
+
+    conversion = 299792458 / 400.8e6
+    delta = np.linspace(-1e-3 * conversion, 1e-3 *
+                        conversion, 200)  # delta p / p
+    phi = np.linspace(-3 * np.pi, 1 * np.pi, 200)
+    DELTA, PHI = np.meshgrid(delta, phi)
+
+    p = p0 * (1.0 + DELTA)  # eV/c
+    E = np.sqrt(p ** 2 + mp ** 2)
+
+    DELTA_E = E / E0 - 1
+
+    H1 = 0.5 * omegaRF * slip * DELTA ** 2
+    H2 = omega0 * V / (2 * np.pi * beta ** 2 * E) * \
+        (np.cos(PHI) - np.cos(phiS) + (PHI - phiS) * np.sin(phiS))
+
+    H = H1 + H2
+
+    PHIp = PHI + np.pi  # above transition energy
+
+    return PHIp * 0.1, DELTA_E, H
+
 def get_ir(ir, s, coord):
     """This function stores the information relevant to the plotting of a specific Interaction Region (IR), 
     i.e. the position of the IR in the accelerator and the limit of the vertical coordinate.
