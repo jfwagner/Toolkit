50ceda4c679323f5f623f51f262df51aeb298b41
Andrea <andrea.stmaria@gmail.com>
2016-04-25 18:20:18 +0200

    Command line arguments added to input particles and jobs

--- scripts/misc/generate_distribution.py
+++ scripts/misc/generate_distribution.py
@@ -1,18 +1,29 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 # ----------------------------------------------------------------------------------------------------------------------------
 # Function to generate a particle distribution as input for the collimation routine in SixTrack (round beams at symmetry point)
 # ----------------------------------------------------------------------------------------------------------------------------
-from random import gauss
-from math import sqrt, pi
+import datetime
 import sys
-from longitudinalDyn import longitudinalHamiltonian 
+import random
+from math import sqrt, pi
+
+import numpy as np
+
+from longitudinalDyn import longitudinalHamiltonian
+
+# Pass the Command Line Arguments
+# --------------------------------------------------------------------------------------------------------------
+particles = int(sys.argv[1])
+jobs = np.int(sys.argv[2])
+
 Hcalculator = longitudinalHamiltonian("HL_coll")
 Hmargin = -0.005  #Hamiltonian to accept below
 
-def dist_generator(particles, emittance, beta, bunch, spread, factor, job):
+def dist_generator(particles, emittance, beta, bunch, spread, factor, job, seed=0):
 
-    seed = '%s'%job
-
+    job_str = '%s'%job
+    # print '>> Generating distribution for job', job
+    
     # Proton mass [eV/c^2], energy [eV], number of particles, relativistic gamma, relativistic beta
     # --------------------------------------------------------------------------------------------------------------
     mp = 0.938272046e9
@@ -20,7 +31,7 @@ def dist_generator(particles, emittance, beta, bunch, spread, factor, job):
     gamma_rel = e_tot/mp
     beta_rel = sqrt(gamma_rel**2 - 1)/gamma_rel
 
-    # Twiss Parameters 
+    # Twiss Parameters
     # --------------------------------------------------------------------------------------------------------------
     gamma = 1/beta
 
@@ -29,62 +40,34 @@ def dist_generator(particles, emittance, beta, bunch, spread, factor, job):
     t_max = sqrt((emittance*beta)/(beta_rel*gamma_rel))	   # beam enveloppe
     tp_max = sqrt(emittance/(beta*beta_rel*gamma_rel))     # beam amplitude
 
-        # f = open('beam_parameters.txt', 'w')
-
-        # for args in (('Number of particles', particles, ''), ('Energy', e_tot, '[eV]'),('Relativistic gamma', gamma_rel, ''), 
-        #              ('Relativistic beta', beta_rel, ''), ('Transverse normalized emittance', emittance, '[m]'),
-        #              ('RMS bunchlength', bunch, '[m]'), ('RMS energy spread', spread, '[%]'), ('Transverse beam envelope', t_max, '[m]'), 
-        #              ('Transverse beam amplitude', tp_max, '[m^-1]'), ('Beta star', beta, '[m]'), ('Sigmas', factor, '[m]')):
-        #     print >> f, '{:<40} {:<40} {:<40}'.format(*args)
-        # f.close()
-
+    # Seeding
     # --------------------------------------------------------------------------------------------------------------
-    # Writing the distribution file
+    if seed == 0:
+        myseed = random.randint(0, 429496729)
+    else:
+        myseed = seed
+    with open('seed.txt', 'a') as g:
+        print >> g,  'job ', job_str ,'seed ', myseed
+    np.random.seed(myseed)
+    random.seed(myseed)
+    
+    # Generating the Transverse Distribution
     # --------------------------------------------------------------------------------------------------------------
-    outfile = 'init_dist_' + seed + '.txt'
-    f = open(outfile,'w')
-
-    ran_x = []
-    ran_xp = []
-    for i in range(0, particles):
-        ran_x.append(gauss(0,1))
-        ran_xp.append(gauss(0,1))
-
-    ran_y = []
-    ran_yp = []
-    for i in range(0, particles):
-        ran_y.append(gauss(0,1))
-        ran_yp.append(gauss(0,1))
-
-    #ran_z = []
-    #ran_e = []
-    #for i in range(0, particles):
-    #        ran_z.append(gauss(0,1))
-    #        ran_e.append(gauss(0,1))
-        #print ran_z
-
-    x = []
-    for particle_x in ran_x:
-        x.append(float(factor*particle_x*t_max))
-    xp = []
-    for particle_xp in ran_xp:
-        xp.append(float(factor*particle_xp*tp_max))
-    y = []
-    for particle_y in ran_y:
-        y.append(float(factor*particle_y*t_max))
-    yp = []
-    for particle_yp in ran_yp:
-        yp.append(float(factor*particle_yp*tp_max))
-
-
+    x = np.random.normal(0, factor*t_max, particles)
+    xp = np.random.normal(0, factor*tp_max, particles)
+    y = np.random.normal(0, factor*t_max, particles)
+    yp = np.random.normal(0, factor*tp_max, particles)
+    
+    # Generating the Longitudinal Distribution
+    # --------------------------------------------------------------------------------------------------------------
     z = []
     E = []
     p0 = sqrt((e_tot-mp)*(e_tot+mp))
-
+    
     while len(z) < particles:
         # Generate for as long time as is needed
-        particle_z = gauss(0,1)
-        particle_e = gauss(0,1)
+        particle_z = random.gauss(0,1)
+        particle_e = random.gauss(0,1)
         trial_z = particle_z*bunch
         trial_e = e_tot*(1 + particle_e*spread) #eV
 
@@ -97,18 +80,21 @@ def dist_generator(particles, emittance, beta, bunch, spread, factor, job):
             #inside bucket; accept!
             z.append(float(trial_z))
             E.append(float(trial_e)*10**(-6))
+            
+    outfile = 'init_dist_' + job_str + '.txt'
+    with open(outfile, 'w') as f:
+        for e1, e2, e3, e4, e5, e6 in zip(x, xp, y, yp, z, E):
+            f.write('%8.6e %8.6e %8.6e %8.6e %8.6e %8.6e\n' % (e1, e2, e3, e4, e5, e6))
 
-
-    for e1, e2, e3, e4, e5, e6 in zip(x, xp, y, yp, z, E):
-        f.write('%8.6e %8.6e %8.6e %8.6e %8.6e %8.6e\n' % (e1, e2, e3, e4, e5, e6))
-
-    f.close()
-
-particles = int(sys.argv[1])
-# (particles, emittance, beta, bunch, spread, factor)
-for number in range(1, 301):
-    n = '%s'%number
-    dist_generator(particles, 2.5e-6, 0.15, 75.5, 1.13e-4, 1, n)
+            
+# Call the function
+# --------------------------------------------------------------------------------------------------------------
+job_range = range(1, int(jobs) + 1)
+with open('seed.txt', 'a') as g:
+    print >> g, datetime.datetime.now()
+for n in job_range:
+    j = '%s'%n
+    dist_generator(particles, 2.5e-6, 0.15, 75.5, 1.13e-4, 1, j, seed=0) # (particles, emittance, beta, bunch, spread, factor, job)
 
 
 
