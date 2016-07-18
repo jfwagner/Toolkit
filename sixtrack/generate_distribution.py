08b4c8efef06609c8d8823d72c4c222d0ced095e
Andrea Santamaria Garcia <andrea.stmaria@gmail.com>
2016-07-18 15:01:29 +0200

    Eliminating any hard coded parameters in the distribution generator. Everything is via command line arguments now.

--- sixtrack/generate_distribution.py
+++ sixtrack/generate_distribution.py
@@ -1,50 +1,48 @@
 #!/usr/bin/env python
 # ----------------------------------------------------------------------------------------------------------------------------
 # Function to generate a particle distribution as input for the collimation routine in SixTrack (round beams at symmetry point)
+# Example for SPS:
+# generate_distribution.py 64 26e9 'SPS_inj' True 1 1 0.9e-6 3.0e-6 1.50895801 -1.392739114 51.8375213 46.54197726 0 0 0.2 14e-4
 # ----------------------------------------------------------------------------------------------------------------------------
 import datetime
 import sys
 import random
-from math import sqrt, pi
 
 import numpy as np
 
 from util import get_bucket
+from util import get_rel_params
+from util import get_sigmas
 
 # Pass the Command Line Arguments
 # --------------------------------------------------------------------------------------------------------------
-particles = int(sys.argv[1])
-jobs = np.int(sys.argv[2])
+particles    = int(sys.argv[1])
+energy       = float(sys.argv[2])
+machine      = sys.argv[3]
+fort13       = sys.argv[4]
+jobs         = int(sys.argv[5])
+factor       = float(sys.argv[6])
+emittance_x  = float(sys.argv[7])
+emittance_y  = float(sys.argv[8])
+alpha_x      = float(sys.argv[9])
+alpha_y      = float(sys.argv[10])
+beta_x       = float(sys.argv[11])
+beta_y       = float(sys.argv[12])
+dispersion_x = float(sys.argv[13])
+dispersion_y = float(sys.argv[14])
+bunch        = float(sys.argv[15])
+spread       = float(sys.argv[16])
 
-def dist_generator(particles, emittance_x, beta_x, alpha_x, emittance_y, beta_y, alpha_y, bunch, spread, factor, job, seed=0, fort13=False):
 
-    job_str = '%s'%job
-    # print '>> Generating distribution for job', job
+def dist_generator(particles, energy, machine, fort13, jobs, factor, emittance_x, emittance_y, alpha_x, alpha_y, beta_x, beta_y, dispersion_x, dispersion_y, bunch, spread, seed=0):
+
+    job_str = '%s'%jobs
     
-    # Proton mass [eV/c^2], energy [eV], number of particles, relativistic gamma, relativistic beta
+    # Getting the Transverse sigmas (amplitudes of phase space ellipse)
     # --------------------------------------------------------------------------------------------------------------
-    c = 2.99792485e8  # m/s
-    mp = 0.938272046e9 # eV/c^2
-    e_tot = 26e9  # eV
-    gamma_rel = e_tot / mp
-    beta_rel = sqrt(gamma_rel**2 - 1) / gamma_rel
-
-    # Twiss Parameter Gamma
-    # --------------------------------------------------------------------------------------------------------------
-    gamma_x = (1 + alpha_x**2)/beta_x
-    gamma_y = (1 + alpha_y**2)/beta_y
-
-    # Standard Deviations, transverse plane x
-    # --------------------------------------------------------------------------------------------------------------
-    dx = -0.1094101846
-    dpx = -0.002663251097
-    tx_max = sqrt((emittance_x*beta_x + dx**2*spread**2)/(beta_rel*gamma_rel))	   # beam enveloppe
-    txp_max = sqrt((emittance_x*gamma_x)/(beta_rel*gamma_rel))
-    
-    # Standard Deviations, transverse plane y
-    # --------------------------------------------------------------------------------------------------------------
-    ty_max = sqrt((emittance_y*beta_y)/(beta_rel*gamma_rel))	# beam enveloppe
-    typ_max = sqrt((emittance_y*gamma_y)/(beta_rel*gamma_rel))   # beam amplitude
+    gamma_rel, beta_rel, p0, mass = get_rel_params(energy)
+    tx_max, txp_max               = get_sigmas(alpha_x, beta_x, emittance_x, dispersion_x, spread, beta_rel, gamma_rel)
+    ty_max, typ_max               = get_sigmas(alpha_y, beta_y, emittance_y, dispersion_y, spread, beta_rel, gamma_rel)
 
     # Seeding
     # --------------------------------------------------------------------------------------------------------------
@@ -59,64 +57,56 @@ def dist_generator(particles, emittance_x, beta_x, alpha_x, emittance_y, beta_y,
     
     # Generating the Transverse Distribution
     # --------------------------------------------------------------------------------------------------------------
-    x_t = np.asarray(np.random.normal(0, factor * tx_max, particles))
+    x_t  = np.asarray(np.random.normal(0, factor * tx_max, particles))
     xp_t = np.asarray(np.random.normal(0, factor * txp_max, particles))
-    y_t = np.asarray(np.random.normal(0, factor * ty_max, particles))
+    y_t  = np.asarray(np.random.normal(0, factor * ty_max, particles))
     yp_t = np.asarray(np.random.normal(0, factor * typ_max, particles))
 
     # Rotating the Transverse Distribution
     # --------------------------------------------------------------------------------------------------------------
     angle_x = np.arctan(-alpha_x/beta_x)
-    x  = x_t*np.cos(angle_x) - xp_t*np.sin(angle_x)
-    xp = x_t*np.sin(angle_x) + xp_t*np.cos(angle_x)
+    x       = x_t*np.cos(angle_x) - xp_t*np.sin(angle_x)
+    xp      = x_t*np.sin(angle_x) + xp_t*np.cos(angle_x)
 
     angle_y = np.arctan(-alpha_y/beta_y)
-    y  = y_t*np.cos(angle_y) - yp_t*np.sin(angle_y)
-    yp = y_t*np.sin(angle_y) + yp_t*np.cos(angle_y)
+    y       = y_t*np.cos(angle_y) - yp_t*np.sin(angle_y)
+    yp      = y_t*np.sin(angle_y) + yp_t*np.cos(angle_y)
     
     # Generating the Longitudinal Distribution
     # --------------------------------------------------------------------------------------------------------------
-    z = []
-    E = []
+    z  = []
+    E  = []
     dp = []
-    p0 = sqrt((e_tot - mp) * (e_tot + mp))
     
     while len(z) < particles:
         # Generate for as long time as is needed
         particle_z = random.gauss(0,1)
         particle_e = random.gauss(0,1)
-        trial_z = particle_z * bunch
-        trial_e = e_tot * (1 + particle_e*spread) #eV
+        trial_z    = particle_z * bunch
+        trial_e    = energy * (1 + particle_e*spread) #eV
 
-        trial_p = sqrt((trial_e - mp) * (trial_e + mp))
-        dPP = (trial_p - p0) / p0
+        trial_p = np.sqrt((trial_e - mass) * (trial_e + mass))
+        dPP     = (trial_p - p0) / p0
+        h       = get_bucket(machine, plot=False, z=trial_z, DELTA=dPP)  # Longitudinal contour
 
-        # print 'dist', trial_z
-
-        h = get_bucket('SPS_inj', plot=False, z=trial_z, DELTA=dPP)  # Longitudinal contour
-        # print h
         Hmargin = -1
-
         if h <= Hmargin:
-            # print trial_z, bunch
-            #inside bucket; accept!
             z.append(float(trial_z))
             E.append(float(trial_e))
             dp.append(dPP)
         else:
-            print 'Trying again,', h
-        # print len(z)
-            
-    zz = np.asarray(z)
-    EE = np.asarray(E)
+            print 'Outside margin, trying again,', h
+  
+    zz  = np.asarray(z)
+    EE  = np.asarray(E)
     ddp = np.asarray(dp)
 
-    if fort13==False:
+    if fort13=='False':
         outfile = 'init_dist_' + job_str + '.txt'
         with open(outfile, 'w') as f:
             for e1, e2, e3, e4, e5, e6 in zip(x, xp, y, yp, z, E):
                 f.write('%8.6e %8.6e %8.6e %8.6e %8.6e %8.6e\n' % (e1, e2, e3, e4, e5, e6))
-    elif fort13==True:
+    elif fort13=='True':
         outfile = 'fort.13'
         with open(outfile, 'w') as f:
             for i in xrange(0, particles, 2):
@@ -134,9 +124,11 @@ def dist_generator(particles, emittance_x, beta_x, alpha_x, emittance_y, beta_y,
                 f.write(str(zz[i+1]*1e3) + "\n") #mm
                 f.write(str(ddp[i+1]) + "\n") #-
 
-                f.write(str(e_tot*1e-6) + "\n") #MeV
+                f.write(str(energy*1e-6) + "\n") #MeV
                 f.write(str(EE[i]*1e-6) + "\n") #MeV
                 f.write(str(EE[i+1]*1e-6) + "\n") #MeV
+    else:
+        print 'Please input True or False in the fourth argument'
                 
 # Call the function
 # --------------------------------------------------------------------------------------------------------------
@@ -145,7 +137,7 @@ with open('seed.txt', 'a') as g:
     print >> g, datetime.datetime.now()
 for n in job_range:
     j = '%s'%n
-    dist_generator(particles, 0.9e-6, 51.8375213, 1.50895801, 3.0e-6, 46.54197726, -1.392739114, 0.2, 14e-4, 1, j, seed=0, fort13=True)
-                 # particles, emittance_x, beta_x, alpha_x, emittance_y, beta_y, alpha_y, bunch, spread 10.7e-4, factor, job, seed=0, fort13=False
+    dist_generator(particles, energy, machine, fort13, jobs, factor, emittance_x, emittance_y, alpha_x, alpha_y, beta_x, beta_y, dispersion_x, dispersion_y, bunch, spread)
+    
 
 
