5be9191a4b232ef89b113c1f2782621b7b34c9f0
Andrea Santamaria Garcia <andrea.stmaria@gmail.com>
2016-07-22 12:54:39 +0200

    Adding different machines to plot_dump

--- sixtrack/dump/plot_dump.py
+++ sixtrack/dump/plot_dump.py
@@ -7,7 +7,6 @@ start = time.time()
 
 import numpy as np
 import matplotlib
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib import font_manager
 from matplotlib import pyplot as plt
@@ -18,31 +17,37 @@ from scipy.stats import gaussian_kde
 
 from util import GetData
 from util import get_bucket
+from util import get_rel_params
+from util import get_sigmas
+from util import get_sigma_ellipse
 from util import get_ellipse_coords
+from util import plot_2d_hist
+from util import plot_sigma
 
 # ------------------------------------------------------------------------------
 # COMMAND LINE ARGUMENTS
 # ------------------------------------------------------------------------------
-infile = sys.argv[1]
-turn_first = int(sys.argv[2])
-turn_last = int(sys.argv[3])
-nbins = int(sys.argv[4])
+infile     = sys.argv[1]
+machine    = sys.argv[2]
+turn_first = int(sys.argv[3])
+turn_last  = int(sys.argv[4])
+nbins      = int(sys.argv[5])
 
 turn_range = range(turn_first, turn_last + 1)
 
 # ------------------------------------------------------------------------------
 # DATA EXTRACTION
 # ------------------------------------------------------------------------------
-get = GetData(infile)
+get  = GetData(infile)
 data = get.data_column()
 
-turns = data[1]
-x_tot = data[3]
+turns  = data[1]
+x_tot  = data[3]
 xp_tot = data[4]
-y_tot = data[5]
+y_tot  = data[5]
 yp_tot = data[6]
-z_tot = data[7]
-e_tot = data[8]
+z_tot  = data[7]
+e_tot  = data[8]
 
 # ------------------------------------------------------------------------------
 # DATA ORGANIZATION IN TERMS OF TURNS
@@ -62,12 +67,12 @@ def get_turns(coord, turns):
         print '>> Dictionary was not correctly created'
     return d
 
-x = get_turns(x_tot, turns)
+x  = get_turns(x_tot, turns)
 xp = get_turns(xp_tot, turns)
-y = get_turns(y_tot, turns)
+y  = get_turns(y_tot, turns)
 yp = get_turns(yp_tot, turns)
-z = get_turns(z_tot, turns)
-e = get_turns(e_tot, turns)
+z  = get_turns(z_tot, turns)
+e  = get_turns(e_tot, turns)
 
 # ------------------------------------------------------------------------------
 # PLOTTING
@@ -92,113 +97,123 @@ rcParams.update(font_spec)
 
 fig = plt.figure()
 
-
-def plot_2d_hist(coord_1, coord_2, nbins):
-    H, xedges, yedges = np.histogram2d(coord_1, coord_2, bins=nbins)
-    H = np.rot90(H)   # H needs to be rotated and flipped
-    H = np.flipud(H)
-    Hmasked = np.ma.masked_where(H == 0, H)  # Mask pixels with a value of zero (they will appear in white)
-    plt.pcolormesh(xedges, yedges, Hmasked, norm=None, vmin=0, vmax=100)
-
-phi, delta, h = get_bucket('SPS_inj')  # Longitudinal contour
-
-# Getting and drawing sigma ellipses
-def get_sigma_ellipse(sigma_x, sigma_y, offset_x, offset_y, number):
-    d = {}
-    for n in range(0, number + 1):
-        d[n] = get_ellipse_coords(a=n*sigma_x, b=n*sigma_y, x=offset_x, y=offset_y, k=1)
-    return d
-
-sigma_x_xp = get_sigma_ellipse(7.16610376198e-06, 4.76160858517e-05, -7.5e-4, 0, 10)
-sigma_y_yp = get_sigma_ellipse(7.12405639107e-06, 4.75602973547e-05, 0, 0.3e-3, 10)
-sigma_z_x = get_sigma_ellipse(0.0151, 7.16610376198e-06, 0, -7.5e-4, 10)
-sigma_z_y = get_sigma_ellipse(0.0151, 7.12405639107e-06, 0, 0, 10)
-sigma_x_y = get_sigma_ellipse(7.16610376198e-06, 7.12405639107e-06, -7.5e-4, 0, 10)
-
-def plot_sigma(my_dict, number):
-    for n in range(0, number + 1):
-        plt.plot(my_dict[n][:,0], my_dict[n][:,1], color='gray', linewidth=0.2)
+if machine=='HL_coll':
+    gamma_rel, beta_rel, p0, mass = get_rel_params(7e12)
     
+    sigma_x, sigma_xp = get_sigmas(0.003485, 0.150739, 2.5e-6, 0.003652, 1.13e-4, beta_rel, gamma_rel)
+    sigma_y, sigma_yp = get_sigmas(-0.000764, 0.150235, 2.5e-6, 0.000517, 1.13e-4, beta_rel, gamma_rel)
+    sigma_z = 0.0755
+    
+    offset_x  = -7.5e-4
+    offset_xp = 0.0
+    offset_y  = 0.0 
+    offset_yp = 0.3e-3
+
+    x_lim  = [-7.9e-4, -7.1e-4]
+    xp_lim = [-3e-4, 3e-4]
+    y_lim  = [-8e-5, 8e-5]
+    yp_lim = [-1e-4, 6e-4]
+    z_lim  = [-0.4, 0.4]
+    e_lim  = [-4.5e-4, 4.5e-4]
+
+elif machine=='SPS_inj':
+    gamma_rel, beta_rel, p0, mass = get_rel_params(26e9)
+    
+    sigma_x, sigma_xp = get_sigmas(1.50895801, 51.8375213, 0.9e-6, 0.0, 10.7e-4, beta_rel, gamma_rel)
+    sigma_y, sigma_yp = get_sigmas(-1.392739114, 46.54197726, 3.0e-6, 0.0, 10.7e-4, beta_rel, gamma_rel)
+    sigma_z = 0.3
+    
+    offset_x  = 0.0
+    offset_xp = 0.0
+    offset_y  = 0.0 
+    offset_yp = 0.0
+
+    x_lim  = [-1e-2, 1e-2]
+    xp_lim = [-0.5e-3, 0.5e-3]
+    y_lim  = [-14e-3, 14e-3]
+    yp_lim = [-4e-4, 4e-4]
+    z_lim  = [-0.8, 0.8]
+    e_lim  = [-8e-3, 8e-3]
+
+else:
+    print '>> Please choose "HL_coll" or "SPS_inj" (2nd argument)'
+
+n_ellipses = 10
+sigma_x_xp = get_sigma_ellipse(sigma_x, sigma_xp, offset_x, offset_xp, n_ellipses)
+sigma_y_yp = get_sigma_ellipse(sigma_y, sigma_yp, offset_y, offset_yp, n_ellipses)
+sigma_z_x  = get_sigma_ellipse(sigma_z, sigma_x, 0, offset_x, n_ellipses)
+sigma_z_y  = get_sigma_ellipse(sigma_z, sigma_y, 0, offset_y, n_ellipses)
+sigma_x_y  = get_sigma_ellipse(sigma_x, sigma_y, offset_x, offset_y, n_ellipses)
+
+
 for turn in turn_range:
-    x_plot = np.asarray(x[turn], dtype='float64') * 1e-3
+    x_plot  = np.asarray(x[turn],  dtype='float64') * 1e-3
     xp_plot = np.asarray(xp[turn], dtype='float64') * 1e-3
-    y_plot = np.asarray(y[turn], dtype='float64') * 1e-3
+    y_plot  = np.asarray(y[turn],  dtype='float64') * 1e-3
     yp_plot = np.asarray(yp[turn], dtype='float64') * 1e-3
-    z_plot = np.asarray(z[turn], dtype='float64') * 1e-3
-    e_plot = np.asarray(e[turn], dtype='float64')
+    z_plot  = np.asarray(z[turn],  dtype='float64') * 1e-3
+    e_plot  = np.asarray(e[turn],  dtype='float64')
 
     ax1 = fig.add_subplot(321)
     plot_2d_hist(x_plot, xp_plot, nbins)
-    # plot_sigma(sigma_x_xp, 10)
+    plot_sigma(sigma_x_xp, 10)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
     ax1.set_xlabel(r"$x$ [m]")
     ax1.set_ylabel(r"$x'$ [m]")
-    # ax1.set_xlim([-8.5e-4, -6.5e-4])
-    # ax1.set_ylim([-5e-4, 5e-4])
-    ax1.set_xlim([-5e-2, 5e-2])
-    ax1.set_ylim([-2e-3, 2e-3])
-
+    ax1.set_xlim(x_lim)
+    ax1.set_ylim(xp_lim)
 
     ax2 = fig.add_subplot(322)
     plot_2d_hist(y_plot, yp_plot, nbins)
-    # plot_sigma(sigma_y_yp, 10)
+    plot_sigma(sigma_y_yp, 10)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
     ax2.set_xlabel(r"$y$ [m]")
     ax2.set_ylabel(r"$y'$ [m]")
-    # ax2.set_xlim([-8e-5, 8e-5])
-    # ax2.set_ylim([-2e-4, 8e-4])
-    ax2.set_ylim([-4e-4, 4e-4])
-    ax2.set_xlim([-12e-3, 12e-3])
+    ax2.set_ylim(yp_lim)
+    ax2.set_xlim(y_lim)
 
     ax3 = fig.add_subplot(323)
     plot_2d_hist(z_plot, x_plot, nbins)
-    # plot_sigma(sigma_z_x, 10)
+    plot_sigma(sigma_z_x, 10)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
     ax3.set_xlabel(r"$z$ [m]")
     ax3.set_ylabel(r"$x$ [m]")
-    # ax3.set_xlim([-0.8, 0.8])
-    # ax3.set_ylim([-8.5e-4, -6.5e-4])
-    ax3.set_xlim([-0.8, 0.8])
-    ax3.set_ylim([-5e-2, 5e-2])
+    ax3.set_xlim(z_lim)
+    ax3.set_ylim(x_lim)
 
     ax4 = fig.add_subplot(324)
     plot_2d_hist(z_plot, y_plot, nbins)
-    # plot_sigma(sigma_z_y, 10)
+    plot_sigma(sigma_z_y, 10)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
     ax4.set_xlabel(r"$z$ [m]")
     ax4.set_ylabel(r"$y$ [m]")
-    # ax4.set_xlim([-0.8, 0.8])
-    # ax4.set_ylim([-8e-5, 8e-5])
-    ax4.set_xlim([-0.8, 0.8])
-    ax4.set_ylim([-12e-3, 12e-3])
+    ax4.set_xlim(z_lim)
+    ax4.set_ylim(y_lim)
 
     ax5 = fig.add_subplot(325)
     plot_2d_hist(x_plot, y_plot, nbins)
-    # plot_sigma(sigma_x_y, 10)
+    plot_sigma(sigma_x_y, 10)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
     ax5.set_xlabel(r"$x$ [m]")
     ax5.set_ylabel(r"$y$ [m]")
-    # ax5.set_xlim([-8.5e-4, -6.5e-4])
-    # ax5.set_ylim([-8e-5, 8e-5])
-    ax5.set_xlim([-5e-2, 5e-2])
-    ax5.set_ylim([-12e-3, 12e-3])
+    ax5.set_xlim(x_lim)
+    ax5.set_ylim(y_lim)
 
+    zz, delta, h = get_bucket(machine)  # Longitudinal contour
     ax6 = fig.add_subplot(326)
     plot_2d_hist(z_plot, e_plot, nbins)
-    plt.contour(2*phi, delta, 2*h, 40, linewidths=0.2, cmap='terrain_r')
+    plt.contour(zz, delta, h, 40, linewidths=0.2, cmap='terrain_r')
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
     ax6.set_ylabel(r"$\Delta E / E$")
     ax6.set_xlabel(r"$z$ [m]")
-    # ax6.set_xlim([-0.8, 0.8])
-    # ax6.set_ylim([-8e-4, 8e-4])
-    ax6.set_xlim([-0.8, 0.8])
-    ax6.set_ylim([-8e-3, 8e-3])
+    ax6.set_xlim(z_lim)
+    ax6.set_ylim(e_lim)
 
 
     plt.suptitle('    Turn {}'.format(turn), fontsize=8, fontweight='bold')
