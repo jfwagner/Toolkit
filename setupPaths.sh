4f6addee5af4a6b9265c633b348e664deda77eeb
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2016-11-09 19:10:32 +0100

    Add the runscripts to the path

--- setupPaths.sh
+++ setupPaths.sh
@@ -6,3 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$PATH:$DIR/sixtrack:$DIR/sixtrack/impacts_real
 export PYTHONPATH=$PYTHONPATH:$DIR
 
+export PATH=$PATH:$DIR/final_runscripts
