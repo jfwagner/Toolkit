8a9910f446691637bec7da3d5d4917e88151f2d3
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2016-11-09 11:57:07 +0100

    Made setupPath.sh script

---
+++ setupPaths.sh
@@ -0,0 +1,8 @@
+#Source this script to setup the paths to use the toolkit!
+
+#Folder of the script (not where it was sourced from)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export PATH=$PATH:$DIR/sixtrack:$DIR/sixtrack/impacts_real
+export PYTHONPATH=$PYTHONPATH:$DIR
+
