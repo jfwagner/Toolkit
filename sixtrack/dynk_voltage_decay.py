d527bf7f8498b6ffeca99ce94d78f24bf09183ee
Andrea Santamaria Garcia <andrea.stmaria@gmail.com>
2016-11-09 16:42:15 +0100

    Adapting the script to one cavity and one tau

--- sixtrack/dynk_voltage_decay.py
+++ sixtrack/dynk_voltage_decay.py
@@ -5,13 +5,13 @@ import os
 
 import numpy as np
 
-max_tau = 5
+max_tau = 4
 taus = []
 for value in range(1, max_tau + 1):
     taus.append(value)
 
-min_turn = 5
-max_turn = 20
+min_turn = 150
+max_turn = 200
 
 turns = []
 for value in range(min_turn + 1, max_turn + 1):
@@ -24,20 +24,20 @@ for value in range(2, min_turn + 1):
 def decay(voltage, turn, min_turn, tau):
     return float(voltage*np.exp(-(turn - min_turn)/tau))
 
-voltages = dict(zip(['ACFCA.AR1.B1', 'ACFCA.BR1.B1', 'ACFCA.CR1.B1', 'ACFCA.DR1.B1'], [2.59386238721, 2.60848248498, 2.70110979319, 2.71695839485]))
+voltages = dict(zip(['voltage'], [1]))
 
-for tau in taus:
-    for key, val in voltages.items():
-        outfile = 'v{}_{}'.format(tau, key)
-        try:
-            os.remove(outfile)
-        except OSError:
-            pass
-        f = open(outfile, 'w')
-        f.write('1 0.0\n')
-        for turn_const in turns_const:
-            f.write('%.0f %.15f\n'%(turn_const, val))
-        for turn in turns:
-            f.write('%.0f %.15f\n'%(turn, decay(val, turn, min_turn, tau)))
-        f.close()
+
+for key, val in voltages.items():
+    outfile = '{}'.format(key)
+    try:
+        os.remove(outfile)
+    except OSError:
+        pass
+    f = open(outfile, 'w')
+    f.write('1 0.0\n')
+    for turn_const in turns_const:
+        f.write('%.0f %.15f\n'%(turn_const, val))
+    for turn in turns:
+        f.write('%.0f %.15f\n'%(turn, decay(val, turn, min_turn, max_tau)))
+    f.close()
 
